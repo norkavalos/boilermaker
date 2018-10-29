508424d01faa6e98a9f21244e9aa2836847babc1
ryuash <ryuash123@gmail.com>
2018-10-29 15:56:02 -0400

    two

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,16 +4,66 @@ class LinkedList {
         this.head=false;
         this.tail=false;
     };
-    addToTail(){
+    addToTail(arg){
+        let newNode = new Node(arg);
+        let oldTail = this.tail;
 
+        if(!this.tail){
+            this.tail=newNode;
+            this.head=this.tail;
+            this.tail.next=null;
+        }
+        else{
+            oldTail.next=newNode;
+            this.tail=newNode;
+            this.tail.next=null;
+            newNode.previous=oldTail;
+        }
     };
-    addToHead(){
-
+    addToHead(arg){
+        let newNode = new Node(arg);
+        let oldHead = this.head;
+        if(!this.head){
+            this.head=newNode;
+            this.tail=this.head;
+            this.head.previous=null;
+        }
+        else{
+            oldHead.previous=newNode;
+            this.head=newNode;
+            this.head.next=oldHead;
+        }
     };
     removeHead(){
+        if(this.head===null && this.tail=== null){
+            return;
+        }
+       if(this.head===this.tail){
+           let holder = this.head;
+           this.head=null;
+           this.tail=null;
+           return holder['value'];
+       }
+
+        let currentHead = this.head;
+        this.head=this.head.next;
+        this.head.previous=null;
+        return currentHead['value'];
 
     };
     removeTail(){
+        if(this.head===this.tail){
+            let holder= this.tail;
+            this.tail=null;
+            this.head=null;
+            return holder['value'];
+        }
+        else{
+            let currentTail = this.tail;
+            this.tail=this.tail.previous;
+            this.tail.next=null;
+            return currentTail['value'];
+        }
 
     };
     search(){
