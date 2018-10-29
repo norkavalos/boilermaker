df3bcd4324e83e73a04e67580dd9e0206c8eb08d
ryuash <ryuash123@gmail.com>
2018-10-29 13:00:43 -0400

    first commit

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,1 +1,30 @@
 // YOUR CODE HERE
+class LinkedList {
+    constructor(){
+        this.head=false;
+        this.tail=false;
+    };
+    addToTail(){
+
+    };
+    addToHead(){
+
+    };
+    removeHead(){
+
+    };
+    removeTail(){
+
+    };
+    search(){
+
+    }
+};
+
+class Node{
+    constructor(arg){
+        this.value=arg;
+        this.next=null;
+        this.previous=null;
+    }
+}
