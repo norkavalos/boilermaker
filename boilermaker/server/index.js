76bab46934d7e896d57922176fc3998b0d17b38a
Norka Avalos <norka.avalos11@gmail.com>
2019-01-03 12:14:09 -0500

    4

--- boilermaker/server/index.js
+++ boilermaker/server/index.js
@@ -1,7 +1,15 @@
 const path = require('path');
 const express = require('express');
+const volleyball = require('volleyball');
+
 const app = express();
 
+// logging middleware
+app.use(volleyball);
+
+// body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, './public')));
 
@@ -9,6 +17,12 @@ app.use(express.static(path.join(__dirname, './public')));
 app.get('*', function (req, res, next) {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
+
+// error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error');
+});
 
 
 module.exports = app;
