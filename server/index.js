b7532fd15e4929257789a7755dfed7b01aa97f63
Norka Avalos <norka.avalos11@gmail.com>
2019-01-05 16:11:05 -0500

    serialize/de part

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const path = require('path');
 const express = require('express');
 const volleyball = require('volleyball');
-
+const db = require('./db');
 const app = express();
 
+const session = require('express-session');
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const dbStore = new SequelizeStore({ db: db });
+
+dbStore.sync();
 // logging middleware
 app.use(volleyball);
-
+app.use('/api', require('./api'));
 // body parsing middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -17,7 +23,19 @@ app.get('*', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-app.use('/api', require('./api'));
+
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'a wildly insecure secret',
+  store: dbStore,
+  resave: false,
+  saveUninitialized: false
+}));
+
+const passport = require('passport');
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 // error handling middleware
 app.use(function (err, req, res, next) {
