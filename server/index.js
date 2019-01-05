27e618adc713851edab8c8e6bc13d3712e6c9995
Norka Avalos <norka.avalos11@gmail.com>
2019-01-05 00:15:37 -0500

    before express

--- server/index.js
+++ server/index.js
@@ -13,16 +13,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, '../public')));
 
-
-app.get('*', function (req, res, next) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+app.get('*', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
 
 // error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(err.status || 500).send(err.message || 'Internal server error');
 });
-
 
 module.exports = app;
